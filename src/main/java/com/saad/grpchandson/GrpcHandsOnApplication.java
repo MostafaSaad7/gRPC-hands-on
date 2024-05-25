0c009b2f9875216a648f8eba9a5550c5c40c2a3a
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-05-25 22:40:00 +0300

    Hello world gRRC application

---
+++ src/main/java/com/saad/grpchandson/GrpcHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.saad.grpchandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcHandsOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcHandsOnApplication.class, args);
+    }
+
+}
